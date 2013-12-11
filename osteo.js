3ee85b31ce6e2a9dec49d5d67e557503d3171c4c
Parker Selbert <parker@sorentwo.com>
2013-12-10 23:12:26 -0600

    Parse keyed responses for collections

--- osteo.js
+++ osteo.js
@@ -43,6 +43,20 @@
 
 (function() {
   Osteo.Collection = Backbone.Collection.extend({
+    initialize: function(_models, options) {
+      if (!options) options = {};
+
+      this.root = options.root;
+    },
+
+    parse: function(response) {
+      if (this.root) {
+        return response[this.root];
+      } else {
+        return response;
+      }
+    },
+
     toPresenters: function(presenter) {
       if (!presenter) presenter = Osteo.Presenter;
 
--- test/collection_test.js
+++ test/collection_test.js
@@ -1,4 +1,13 @@
 describe('Osteo.Collection', function() {
+  describe('#parse', function() {
+    it('extracts data from the root key', function() {
+      var collection = new Osteo.Collection([], { root: 'posts' }),
+          payload    = { posts: [{ id: 1 }, { id: 2 }] };
+
+      expect(collection.parse(payload).length).to.eq(2);
+    });
+  });
+
   describe('#toPresenters', function() {
     it('generates an array of presenter wrapped models', function() {
       var collection = new Osteo.Collection([

c596278811d3e9890c2ab02337fd8ddaf918dc14
Parker Selbert <parker@sorentwo.com>
2014-01-08 10:54:18 -0600

    Implement a route reset function

--- osteo.js
+++ osteo.js
@@ -171,6 +171,10 @@ Presenter.extend = Backbone.Model.extend;
 var Route = Osteo.Route = function() {
 };
 
+Route.reset = function() {
+  this.instance = undefined;
+};
+
 _.extend(Route.prototype, {
   load: function() {
     return this;
--- test/route_test.js
+++ test/route_test.js
@@ -1,4 +1,13 @@
 describe('Osteo.Route', function() {
+  describe('.reset', function() {
+    it('clears the singleton instance', function() {
+      Osteo.Route.instance = {};
+      Osteo.Route.reset();
+
+      expect(Osteo.Route.instance).to.be.undefined;
+    });
+  });
+
   describe('#load', function() {
     it('is defaults to a no-op', function() {
       var route = new Osteo.Route();
